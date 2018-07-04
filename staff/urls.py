658597b02ec0585bfe9a57813b162a173c97dc95
taotie <760688349@qq.com>
2018-07-04 16:53:26 +0800

    增加部门信息的删除功能

--- staff/urls.py
+++ staff/urls.py
@@ -4,12 +4,13 @@ from django.conf.urls import url
 from staff import views
 
 urlpatterns = [
-    url(r'emp_list.html', views.emp_list),
+    url(r'^emp_list.html', views.emp_list),
     url(r'house_list.html', views.house_list),
     url(r'house_type_list.html', views.house_type_list),
     url(r'dept_list.html', views.dept_list),
     url(r'notice_list.html', views.notice_list),
-    url(r'delete/',views.house_type_delete)
+    url(r'^delete/',views.house_type_delete),
+    url(r'^d_delete/',views.d_del_view)
 
 
 
--- staff/views.py
+++ staff/views.py
@@ -33,7 +33,18 @@ def notice_list(request):
 
 # 删除房屋类型操作
 def house_type_delete(request):
-        cno = request.GET.get('cno', '')
-        cn = int(cno)
-        HouseType.objects.filter(type_id=cn).delete()
-        return HttpResponseRedirect('/staff/house_type_list.html')
+    print "fangwu"
+    cno = request.GET.get('cno', '')
+    cn = int(cno)
+    HouseType.objects.filter(type_id=cn).delete()
+    return HttpResponseRedirect('/staff/house_type_list.html')
+
+#删除部门信息操作
+def d_del_view(request):
+    print 'sdlkflkjls'
+    #获取部门信息
+    dell = request.GET.get('dell','0')
+    print dell
+    dell = int(dell)
+    DepartmentInfo.objects.filter(department_id= dell).delete()
+    return HttpResponseRedirect('/staff/Dedept_list.html/')
