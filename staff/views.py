2ce50e262ae4ffddfa6b9f2fbbf7467a0aa341b3
taotie <760688349@qq.com>
2018-07-04 15:59:45 +0800

    重新加载房屋信息、房屋类型、部门信息列表

--- staff/views.py
+++ staff/views.py
@@ -2,22 +2,28 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from home.models import *
 
 # Create your views here.
 def emp_list(request):
     return render(request, 'emp_list.html')
 
-
+# 房屋信息
 def house_list(request):
-    return render(request, 'house_list.html')
+    houseinfo = HouseInfo.objects.all()
+    return render(request, 'house_list.html',{"houseinfo":houseinfo})
 
-
+# 房屋类型
 def house_type_list(request):
-    return render(request, 'house_type_list.html')
+    housetype = HouseType.objects.all()
+    # return render(request, 'Toh.html', {'housetype': housetype})
+    return render(request, 'house_type_list.html',{'housetype': housetype})
 
-
+# 部门信息
 def dept_list(request):
-    return render(request, 'dept_list.html')
+    department = DepartmentInfo.objects.all()
+    # return render(request, 'Dept.html', {'department': department})
+    return render(request, 'dept_list.html',{'department': department})
 
 
 def notice_list(request):
